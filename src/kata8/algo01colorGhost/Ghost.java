16fd4180d29fdc160d66dfb619863a152896b421
Olugbodi Johnny <olugbodijohnny@live.com>
2022-08-06 19:23:28 +0100

    Color ghost algo solution

---
+++ src/kata8/algo01colorGhost/Ghost.java
@@ -0,0 +1,11 @@
+package kata8.algo01colorGhost;
+
+public class Ghost {
+    // your code goes here
+    private String[] colorArray = new String[]{"white","yellow","purple","red"};
+    private String color = colorArray[(int) (Math.random() * (3 + 1))];
+
+    public String getColor () {
+        return color;
+    }
+}
