cd4a008fc9e587155b40497d636320be38414c4f
Olugbodi Johnny <olugbodijohnny@live.com>
2022-08-07 22:10:27 +0100

    Localize The Barycenter of a Triangle algo solution

---
+++ src/kata8/algo05LocalizeTheBarycenterOfATriangle/Barycenter.java
@@ -0,0 +1,13 @@
+package kata8.algo05LocalizeTheBarycenterOfATriangle;
+
+class Barycenter {
+
+    public static double[] barTriang(double[] x, double[] y, double[] z) {
+        // your code
+        double xCentroidCoordinate = ((x[0] + y[0] + z[0]) / 3);
+        xCentroidCoordinate = Math.round(xCentroidCoordinate*Math.pow(10,4))/Math.pow(10,4);
+        double yCentroidCoordinate = ((x[1] + y[1] + z[1]) / 3);
+        yCentroidCoordinate = Math.round(yCentroidCoordinate*Math.pow(10,4))/Math.pow(10,4);
+        return new double[]{xCentroidCoordinate,yCentroidCoordinate};
+    }
+}
